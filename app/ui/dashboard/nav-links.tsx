d4d2e10957d8a6706afd0ea3d6d680f16067c4e1
DESCARAVAGE <escaravage.daniel@gmail.com>
2026-04-21 10:52:00 +0200

    First commit
    Signed-off-by: DESCARAVAGE <escaravage.daniel@gmail.com>

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -3,6 +3,7 @@ import {
   HomeIcon,
   DocumentDuplicateIcon,
 } from '@heroicons/react/24/outline';
+import { inter } from '../fonts';
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
@@ -28,7 +29,7 @@ export default function NavLinks() {
             className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
           >
             <LinkIcon className="w-6" />
-            <p className="hidden md:block">{link.name}</p>
+            <p className={`${inter.className} hidden md:block`}>{link.name}</p>
           </a>
         );
       })}
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from 'next';
+import path from 'path';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  turbopack: {
+    root: path.join(__dirname), // Or path.resolve(__dirname, '../../') for monorepo apps
+  },
 };
 
 export default nextConfig;
