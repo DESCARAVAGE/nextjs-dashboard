d3891f79edfb15a2afdbfb23c68aa4a874bea5e6
DESCARAVAGE <escaravage.daniel@gmail.com>
2026-05-12 15:04:13 +0200

    Handling Error
    Signed-off-by: DESCARAVAGE <escaravage.daniel@gmail.com>

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -6,16 +6,16 @@ import postgres from 'postgres';
 import { redirect } from 'next/navigation';
 
 //Création du lien avec la BDD
-const sql = postgres(process.env.POSTGRES_URL!, {ssl: 'require'});
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 //Définition de l'objet attendu dans le formData
 const FormSchema = z.object({
-    id: z.string(),
-    customerId: z.string(),
-    // coerce = contraindre un input en un autre type.
-    amount: z.coerce.number(),
-    status: z.enum(['pending', 'paid']),
-    date: z.string(),
+  id: z.string(),
+  customerId: z.string(),
+  // coerce = contraindre un input en un autre type.
+  amount: z.coerce.number(),
+  status: z.enum(['pending', 'paid']),
+  date: z.string(),
 });
 
 //Omission des champs id et date
@@ -25,26 +25,34 @@ const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
 
 export async function createInvoice(formData: FormData) {
-    const { customerId, amount, status } = CreateInvoice.parse({
-        customerId: formData.get('customerId'),
-        amount: formData.get('amount'),
-        status: formData.get('status'),
-    });
-    // console.log(typeof rawFormData.amount);
+  const { customerId, amount, status } = CreateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+  // console.log(typeof rawFormData.amount);
 
-    //Convertission en cents
-    const amountInCents = amount * 100;
-    //Définition du format de date
-    const date = new Date().toISOString().split('T')[0];
-
+  //Convertission en cents
+  const amountInCents = amount * 100;
+  //Définition du format de date
+  const date = new Date().toISOString().split('T')[0];
+  try {
     //Insertion en BDD
     await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
 
-    revalidatePath('/dashboard/invoices');
-    redirect('/dashboard/invoices');
+  } catch (error) {
+    // We'll also log the error to the console for now
+    console.error(error);
+    return {
+      message: 'Database Error: Failed to Create Invoice',
+    };
+  };
+
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
 };
 
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
@@ -55,20 +63,30 @@ export async function updateInvoice(id: string, formData: FormData) {
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
- 
+
   const amountInCents = amount * 100;
- 
+  try {
   await sql`
     UPDATE invoices
     SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
     WHERE id = ${id}
   `;
- 
+  } catch (error) {
+    // We'll also log the error to the console for now
+    console.error(error);
+    return {
+      message: 'Database Error: Failed to Update Invoice',
+    };
+  }
+
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
 
 export async function deleteInvoice(id: string) {
+  throw new Error('Failed to Delete Invoice');
+ 
+  // Unreachable code block
   await sql`DELETE FROM invoices WHERE id = ${id}`;
   revalidatePath('/dashboard/invoices');
 }
